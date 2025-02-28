1acc96eb30dc68b877991c3f7184d5a2efeb0a7c
RanisKh <ranis.rafisovich@bk.ru>
2025-02-28 22:30:39 +0300

    Strings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("1 task");
+    String firstName = "Ivan";
+    String middleName =  "Ivanov";
+    String lastName = "Ivanovich";
+    String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника —" + fullName);
+
+        System.out.println("2 task");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —" + fullName.toUpperCase());
+
+        System.out.println("3 task");
+        String fulName = "Иванов Семён Семёнович";
+        String changed = fulName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника —" + changed);
     }
 }
